a198fb14e91b468d9dabab142d0f68b8808b5f07
Frank Adcock <frank@bushlife.com.au>
2014-04-10 03:05:58 +0000

    Update FXCop batch script
    git-svn-id: https://svn.ho.bushlife.com.au/svn/bl_cs_common/trunk@29 6347b29f-6786-4e24-9912-a3462b3e71b8

--- JenkinsBatchFiles/BushLifeCommonFXCop.bat
+++ JenkinsBatchFiles/BushLifeCommonFXCop.bat
@@ -4,6 +4,8 @@ REM
 REM
 set PROGRAMFILES=C:\Program Files (x86)
 "%PROGRAMFILES%\Microsoft FxCop 1.36\FxCopCmd" /ignoregeneratedcode /f:"%WORKSPACE%\BushLifeCommon\bin\Debug\BushLifeCommon.dll" /out:fxcop.xml /fo /rule:"%PROGRAMFILES%\Microsoft FxCop 1.36\Rules" /rid:-"Microsoft.Design#CA1004" /rid:-"Microsoft.Design#CA1006" /rid:-"Microsoft.Design#CA1026" /rid:-"Microsoft.Design#CA1031" /rid:-"Microsoft.Globalization#CA1300" /rid:-"Microsoft.Globalization#CA1305" /rid:-"Microsoft.Performance#CA1800" /rid:-"Microsoft.Usage#CA2213" /d:"%PROGRAMFILES%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /d:"%WORKSPACE%\SharedLibs\x64\Hibernate" /gac  /dictionary:"%WORKSPACE%\BushLifeCommon\FXCopDictionary.xml"
-if %ERRORLEVEL%==0 or %ERRORLEVEL%==512 exit /b 1
+set RETVAL=%ERRORLEVEL%
+if %RETVAL%==0 exit /b 1
+if %RETVAL%==512 exit /b 1
 exit /b 0
 
