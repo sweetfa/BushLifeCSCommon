7d8611bdd53c210927edd30935e2e2516c980aca
Frank Adcock <frank@bushlife.com.au>
2015-03-08 02:45:02 +0000

    Fix unit test execution
    git-svn-id: https://svn.ho.bushlife.com.au/svn/bl_cs_common/trunk@42 6347b29f-6786-4e24-9912-a3462b3e71b8

--- JenkinsBatchFiles/BushLifeCommonUnitTest.bat
+++ JenkinsBatchFiles/BushLifeCommonUnitTest.bat
@@ -9,7 +9,7 @@ mkdir "%WORKSPACE%\TestResults"
 
 set EXITCODE=0
 rmdir /S /Q "%ASSEMBLYDIR%\TestResources"
-xcopy /i /e "%WORKSPACE%\src\NOESTest\TestResources" "%ASSEMBLYDIR%\TestResources"
+xcopy /i /e "%WORKSPACE%\BushLifeCommonTest\TestResources" "%ASSEMBLYDIR%\TestResources"
 call "C:\Program Files (x86)\Typemock\Isolator\7.1\mocking_on.bat"
 "c:\Program Files\Gallio\bin\Gallio.Echo.exe" "%ASSEMBLYDIR%\BushLifeCommonTest.dll" /report-directory:"%WORKSPACE%\TestResults" /report-type:Xml /working-directory:"%WORKSPACE%"
 IF ERRORLEVEL 1 SET EXITCODE=1
