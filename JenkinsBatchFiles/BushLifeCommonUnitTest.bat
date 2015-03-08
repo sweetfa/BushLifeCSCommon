b31be693017868a8363296198f13a7a8d4d04004
Frank Adcock <frank@bushlife.com.au>
2015-03-08 01:43:22 +0000

    Fix unit test execution
    git-svn-id: https://svn.ho.bushlife.com.au/svn/bl_cs_common/trunk@41 6347b29f-6786-4e24-9912-a3462b3e71b8

--- JenkinsBatchFiles/BushLifeCommonUnitTest.bat
+++ JenkinsBatchFiles/BushLifeCommonUnitTest.bat
@@ -2,14 +2,16 @@ REM
 REM	Batch file to run unit tests for BushLifeCommon library
 REM
 REM
-dir "%WORKSPACE%\TestResults"
-set EXITCODE=0
+
+set ASSEMBLYDIR=%WORKSPACE%\BushLifeCommonTest\bin\Debug
+
 mkdir "%WORKSPACE%\TestResults"
-rmdir /S /Q "%WORKSPACE%\BushLifeCommonTest\bin\Debug\TestResources"
-xcopy /i /e "%WORKSPACE%\BushLifeCommonTest\TestResources" "%WORKSPACE%\BushLifeCommonTest\bin\Debug\TestResources"
+
+set EXITCODE=0
+rmdir /S /Q "%ASSEMBLYDIR%\TestResources"
+xcopy /i /e "%WORKSPACE%\src\NOESTest\TestResources" "%ASSEMBLYDIR%\TestResources"
 call "C:\Program Files (x86)\Typemock\Isolator\7.1\mocking_on.bat"
-"c:\Program Files\Gallio\bin\Gallio.Echo.exe" "%WORKSPACE%\BushLifeCommonTest\bin\Debug\BushLifeCommonTest.dll" /report-directory:"%WORKSPACE%\TestResults" /report-type:Xml /working-directory:"%WORKSPACE%"
+"c:\Program Files\Gallio\bin\Gallio.Echo.exe" "%ASSEMBLYDIR%\BushLifeCommonTest.dll" /report-directory:"%WORKSPACE%\TestResults" /report-type:Xml /working-directory:"%WORKSPACE%"
 IF ERRORLEVEL 1 SET EXITCODE=1
 call "C:\Program Files (x86)\Typemock\Isolator\7.1\mocking_off.bat"
 exit /b %EXITCODE%
-
